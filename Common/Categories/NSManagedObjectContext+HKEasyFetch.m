bdba18bb6ebb4915d461f8c02eb2a47656d8b269
Gustaf Lindqvist <gustaf@gustaf.nu>
2011-06-29 15:43:16 +0200

    Easy fetch from a managed object context (taken from Cocoa with Love)

---
+++ Common/Categories/NSManagedObjectContext+HKEasyFetch.m
@@ -0,0 +1,71 @@
+//  Copyright (c) 2011 HoboCode
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "NSManagedObjectContext+HKEasyFetch.h"
+
+// taken from: http://cocoawithlove.com/2008/03/core-data-one-line-fetch.html
+
+@implementation NSManagedObjectContext (HKEasyFetch)
+
+- (NSSet *)fetchObjectsForEntityName:(NSString *)newEntityName
+                       withPredicate:(id)stringOrPredicate, ...
+{
+    NSEntityDescription *entity = [NSEntityDescription entityForName:newEntityName inManagedObjectContext:self];
+    NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+    [request setEntity:entity];
+    
+    if (stringOrPredicate)
+    {
+        NSPredicate *predicate;
+        if ( [stringOrPredicate isKindOfClass:[NSString class]] )
+        {
+            va_list variadicArguments;
+            va_start(variadicArguments, stringOrPredicate);
+            predicate = [NSPredicate predicateWithFormat:stringOrPredicate
+                                               arguments:variadicArguments];
+            va_end(variadicArguments);
+        }
+        else
+        {
+            NSAssert2( [stringOrPredicate isKindOfClass:[NSPredicate class]],
+                      @"Second parameter passed to %s is of unexpected class %@",
+                      sel_getName(_cmd), [stringOrPredicate className]);
+            predicate = (NSPredicate *)stringOrPredicate;
+        }
+
+        [request setPredicate:predicate];
+    }
+    
+    NSError *error = nil;
+    NSArray *results = [self executeFetchRequest:request error:&error];
+
+    if (error != nil)
+    {
+        [NSException raise:NSGenericException format:[error description]];
+    }
+    
+    return [NSSet setWithArray:results];
+}
+
+@end
